6e80712d91f2a81db25d9167a539b93349948bd9
Luca D'Amico <menteora@gmail.com>
2018-03-14 14:09:30 +0100

    feat: add service worker

--- js/main.js
+++ js/main.js
@@ -159,9 +159,9 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
-  return li
+  return li;
 }
 
 /**
@@ -176,4 +176,64 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
+}
+
+/**
+ * Service Worker Check
+ */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then((reg) => {
+      if (!navigator.serviceWorker.controller) {
+        return;
+      }
+
+      if (reg.waiting) {
+        console.log('SW waiting')
+        var worker = reg.waiting;
+        worker.postMessage({
+          action: 'skipWaiting'
+        });
+        return;
+      }
+
+      if (reg.installing) {
+        console.log('SW installing')
+        var worker = reg.installing;
+        worker.addEventListener('statechange', () => {
+          if (worker.state == 'installed') {
+            console.log('SW installed')
+            worker.postMessage({
+              action: 'skipWaiting'
+            });
+          }
+        });
+        return;
+      }
+
+      reg.addEventListener('updatefound', () => {
+        console.log('SW update found')
+        var worker = reg.installing;
+        worker.addEventListener('statechange', () => {
+          if (worker.state == 'installed') {
+            console.log('SW installed')
+            worker.postMessage({
+              action: 'skipWaiting'
+            });
+          }
+        });
+      });
+  });
+
+  var refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', function() {
+    console.log('SW controllerchange')
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
+
+  if (!navigator.onLine) {
+    console.log('offline');
+    document.getElementById('map-container').style.display = "none";
+  }
 }
---
+++ sw.js
@@ -0,0 +1,70 @@
+var staticCacheName = 'mws-restourant-static-v20';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+        '/index.html',
+        '/css/styles.css',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/data/restaurants.json',
+        'restaurant.html?id=1',
+        'restaurant.html?id=2',
+        'restaurant.html?id=3',
+        'restaurant.html?id=4',
+        'restaurant.html?id=5',
+        'restaurant.html?id=6',
+        'restaurant.html?id=7',
+        'restaurant.html?id=8',
+        'restaurant.html?id=9',
+        'restaurant.html?id=10'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restourant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+    console.log('SW skip waiting')
+  }
+});
