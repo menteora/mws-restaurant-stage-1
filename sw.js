fa9e0cb20a4635abc9066c68c44a2567041ee83d
Luca D'Amico <menteora@gmail.com>
2018-03-20 11:55:49 +0100

    fix: re-add name of cache, to ditribute update on content update

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
+var currentCacheName = 'mws-restourant-dynamic-v01';
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restourant-') &&
+                 cacheName != currentCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.open('mws-restourant-dynamic').then(function (cache) {
+    caches.open(currentCacheName).then(function (cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
