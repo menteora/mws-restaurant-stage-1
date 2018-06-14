4b4de9529b6b3f6d452a4ae8646fd19ec8db03c7
Luca D'Amico <menteora@gmail.com>
2018-06-14 15:19:24 +0200

    fix: clean code and add comment

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/idb.js');
 importScripts('js/dbhelper.js');
 
-var currentCacheName = 'mws-restaurant-dynamic-v38';
+var currentCacheName = 'mws-restaurant-dynamic-v40';
 var apiUrl = 'http://localhost:1337/restaurants';
 
 self.addEventListener('activate', function (event) {
@@ -19,26 +19,10 @@ self.addEventListener('activate', function (event) {
   );
 });
 self.addEventListener('fetch', function (event) {
-  // external api url bypass service worker cache
-  //if (event.request.url.indexOf(apiUrl) !== -1) {
-  /*
-  idb.open('restaurants', 1, upgradeDb => {
-    upgradeDb.createObjectStore('restaurants', {
-      keyPath: 'id'
-    });
-  }).then(function (db) {
-    var tx = db.transaction('restaurants')
-    var store = tx.objectStore('restaurants');
-    store.getAll().then(restaurants => {
-      return new Response(JSON.stringify(restaurants), { "status": 200, "statusText": "MyCustomResponse!" });
-    });
-  });
-  */
-  //}
-  //console.log("FIST URL:" + event.request.url);
+  // check if page is a restaurants data
   if (event.request.url.indexOf(apiUrl) !== -1) {
-    console.log("SECOND URL:" + event.request.url);
     event.respondWith(
+      // open db
       idb.open('restaurants', 1, upgradeDb => {
         upgradeDb.createObjectStore('restaurants', {
           keyPath: 'id'
@@ -46,26 +30,29 @@ self.addEventListener('fetch', function (event) {
       }).then(function (db) {
         var tx = db.transaction('restaurants')
         var store = tx.objectStore('restaurants');
-        return store.getAll().then(function (restaurants) {
-          console.log("FORTH URL:" + JSON.stringify(restaurants));
-          console.log("FORTH LENGHT:" + JSON.stringify(restaurants.length));
-          return (restaurants.length != 0) ?  new Response(JSON.stringify(restaurants),  { "status": 200, headers: { 'Content-Type': 'application/json' }}) : fetch(event.request).then(function (response) {
+        // get all restaurants inside local db
+        return store.getAll().then(function (restaurantsOffline) {
+          return fetch(event.request).then(function (response) {
             return response.json();
-          }).then(function (restaurants) {
+          }).then(function (restaurantsOnline) {
             // fetch online
-            console.log("fetch restaurants online")
+            console.log("fetch restaurants online");
             var tx = db.transaction('restaurants', 'readwrite')
             var store = tx.objectStore('restaurants');
-            restaurants.forEach(function (restaurant) {
+            // update restaurants
+            restaurantsOnline.forEach(function (restaurant) {
               store.put(restaurant);
             });
-            //cache.put(event.request, response.clone());
-            return new Response(JSON.stringify(restaurants), { "status": 200, headers: { 'Content-Type': 'application/json' }});
-          })
+            return new Response(JSON.stringify(restaurantsOnline), { "status": 200, headers: { 'Content-Type': 'application/json' } });
+          }).catch(function (error) {
+            // fetch offline
+            console.log("fetch restaurants offline");
+            return new Response(JSON.stringify(restaurantsOffline), { "status": 200, headers: { 'Content-Type': 'application/json' } })
+          });
         })
       })
     );
-    // Prevent two fetches
+    // prevent two fetches
     return;
   }
   //console.log("THIRD URL:" + event.request.url);
